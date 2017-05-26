4fa31d5921110979ddaf8d439a9c891e882a6ad9
Ben Word <ben@benword.com>
2017-05-26 11:11:23 -0600

    Add plugin headers - necessary for Bedrock mu-plugins autoloader

--- kinsta-mu-plugins.php
+++ kinsta-mu-plugins.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Plugin Name:  kinsta-mu-plugins
+Version:      1.0.0
+*/
+
 define( 'KINSTAMU_VERSION', '1.0.0' );
 require( 'kinsta-mu-plugins/admin-text-modifications/admin-text-modifications.php' );
 require( 'kinsta-mu-plugins/ip-ban/ip-ban.php' );
